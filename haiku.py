929ecdd02521c24b5e4f7404f1f316d54ff10a41
julieherrick <julieherrick@users.noreply.github.com>
2017-06-23 08:53:24 -0500

    Add files via upload

---
+++ haiku.py
@@ -0,0 +1,8 @@
+import random
+
+ln1 = ["Haikus are easy", "You never feed me", "Wanna go outside", "The rule for today:", "Ask for opinions", "I think haikus suck", "*Five syllables here*", "A hypotenuse", "Space is limited", "Help me, I am trapped", "Are you kidding me", "I will have you know", "First I was afraid", "Questions and answers", "We arrived too late", "I do not like you", "Haikus confuse me", ] #5 syllables
+ln2 = ["Prehaps I'll sleep on your face", "Must attack at once", "Your house is a giant mess", "OH NO! I am in a tree", "*Seven more syllables here*", "No no no no no no no", "Individuality", "Oversimplification", "Danger lies at every turn", "Oh my gosh look at the duck", "I am going to end you", "Nationwide is on your side", "You are not my real father", "Alexander Hamilton", ] #3 syllables
+ln3 = ["refrigerator", "Just do what you want", "I think haikus suck", "Life is so extreme", "Who came up with this?", "You can use your fingers", "OMG Kittens", "Are you happy now?", "Can't wait to decide", "Please save me now", "Confusion abounds", "*Five syllables here*", "Short. Sweet. To the point.", "That was horrible", "I want to sleep now", "Is this poetry?", "Are you kidding me", "A fire is hot", "I will use fingers",] #5 syllables
+print(random.choice(ln1))
+print(random.choice(ln2))
+print(random.choice(ln3))
